fdd388537b89c0ea182657357c85bb20742bd348
Teng <icescaicemice@gmail.com>
2026-03-04 20:56:42 +0800

    add generator part

--- reducing_op2.py
+++ reducing_op2.py
@@ -1,12 +1,11 @@
 import timeit
 
-
+"""學習使用 timeit，並瞭解什麼搜尋方法，資料在有序的情形下，資料在前及在後，用了多少時間。"""
 def search_fast(haystack, needle):
     for item in haystack:
         if item == needle:
             return True
     return False
-
 
 def search_slow(haystack, needle):
     """Full Scan"""
@@ -16,25 +15,32 @@ def search_slow(haystack, needle):
             return_value = True
     return return_value
 
+def search_generator(haystack, needle):
+    """有任何 generator 生成布林值為真"""
+    return any((item == needle for item in haystack))
 
 def search_tuple(haystack, needle):
     """有任何 tuple 值組布林值為真"""
-    return any((item == needle for item in haystack))
-
+    return any(tuple(item == needle for item in haystack))
 
 def search_list(haystack, needle):
     """有任何 list 串列布林值為真"""
     return any([item == needle for item in haystack])
 
-
 if __name__ == "__main__":
+    # 使用 timeit 執行的次數
     iterations = 10000
+    # 建立 有序的串列 1000 個
     haystack = list(range(1000))
-    setup = "from __main__ import (haystack, needle, search_fast, search_slow, search_tuple, search_list)"
+    # 為 timeit 做設定
+    setup = "from __main__ import (haystack, needle, search_fast, search_slow, search_tuple, search_list, search_generator)"
     print(setup)
+
+    # 設定尋找的值 資料在前
     needle = 5
-    print(f"tuple = {(item == needle for item in haystack)}")
-    print(f"list = {[item == needle for item in haystack]}")
+    print(f"generator = {(item == needle for item in haystack)}")
+    print(f"type of (item == needle for item in haystack) = {type(item == needle for item in haystack)}")
+
     print(
         f"Testing search speed with {len(haystack)} items and needle close to the head of the list"
     )
@@ -50,15 +56,21 @@ if __name__ == "__main__":
     print(f"search_slow time: {t/iterations:.5e}")
 
     t = timeit.timeit(
-        stmt="search_tuple(haystack, needle)", setup=setup, number=iterations
+        stmt="search_generator(haystack, needle)", setup=setup, number=iterations
     )
-    print(f"search_tuple time: {t/iterations:.5e}")
+    print(f"search_generator time: {t/iterations:.5e}")
 
     t = timeit.timeit(
         stmt="search_list(haystack, needle)", setup=setup, number=iterations
     )
     print(f"search_list time: {t/iterations:.5e}")
 
+    t = timeit.timeit(
+        stmt="search_tuple(haystack, needle)", setup=setup, number=iterations
+    )
+    print(f"search_tuple time: {t/iterations:.5e}")
+
+    # 設定尋找的值 資料在後
     needle = len(haystack) - 10
     print(
         f"Testing search speed with {len(haystack)} items and needle close to the tail of the list"
@@ -75,11 +87,16 @@ if __name__ == "__main__":
     print(f"search_slow time: {t/iterations:.5e}")
 
     t = timeit.timeit(
-        stmt="search_tuple(haystack, needle)", setup=setup, number=iterations
+        stmt="search_generator(haystack, needle)", setup=setup, number=iterations
     )
-    print(f"search_tuple time: {t/iterations:.5e}")
+    print(f"search_generator time: {t/iterations:.5e}")
 
     t = timeit.timeit(
         stmt="search_list(haystack, needle)", setup=setup, number=iterations
     )
     print(f"search_list time: {t/iterations:.5e}")
+
+    t = timeit.timeit(
+        stmt="search_tuple(haystack, needle)", setup=setup, number=iterations
+    )
+    print(f"search_tuple time: {t/iterations:.5e}")
