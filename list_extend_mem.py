8c666b10e31444e7ee26ecea86495bbbc27a501a
Teng <icescaicemice@gmail.com>
2026-03-15 17:11:13 +0800

    begin ch6

---
+++ list_extend_mem.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 17 14:18:59 2025
+
+@author: jos
+"""
+import time
+from functools import wraps
+
+if 'line_profiler' not in dir() and 'profile' not in dir():
+    def profile(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            return func(*args, **kwargs)
+        return inner
+
+
+@profile
+def list_extend(y, y1, i = 1000):
+    for o in range(i):
+        y.extend(y1)
+        time.sleep(0.002)
+    return y
+
+
+def main():
+    y = [x for x in range(10)]
+    y1 = [x for x in range(5)]
+    print(y)
+    print(y1)
+    y = list_extend(y, y1, 1000)
+    print(y[:40])
+
+
+if __name__ == "__main__":
+    main()
---
+++ namespace_dis.py
@@ -0,0 +1,6 @@
+import dis
+import namespace as ns
+
+dis.dis(ns.test1)
+dis.dis(ns.test2)
+dis.dis(ns.test3)
